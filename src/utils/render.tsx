2c26d7f82b45fed3e0080f22537c2da765f4c6cb
Fabien Picard <pic.fab@hotmail.fr>
2024-09-09 22:35:28 +0200

    first

--- src/utils/render.tsx
+++ src/utils/render.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 
 export function render(component: React.ReactNode): void {
-  root.render(<StrictMode>{component}</StrictMode>);
+	root.render(<StrictMode>{component}</StrictMode>);
 }
